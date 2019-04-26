61b91b5d74a2369fec3ec06a59e57e16b4cf30e0
LeslieZarate <fiorelly30zarate@gmail.com>
2019-04-26 11:24:23 -0500

    Funcion init

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+const init = () =>{
+  const config = {
+      apiKey: "AIzaSyCiKAaY7lb-RFBBp10RSyTZmRHd1BBo90w",
+      authDomain: "db-drama-fever.firebaseapp.com",
+      databaseURL: "https://db-drama-fever.firebaseio.com",
+      projectId: "db-drama-fever",
+      storageBucket: "db-drama-fever.appspot.com",
+      messagingSenderId: "82992397174"
+    };
+    firebase.initializeApp(config);  
+}
+ init();
+
 // LOGIN CON GOOGLE
 const  provider = new firebase.auth.GoogleAuthProvider();
 const authGoogle = document.getElementById('auth-google');  // boton 
