4eb69ace4060fd5829b37123314ba4c9e0317a6d
Daianatk <daiana.campos04@gmail.com>
2019-05-01 18:21:15 -0500

    Vista Home

--- src/view/account.js
+++ src/view/account.js
@@ -1,4 +1,4 @@
-import{signUpSubmit} from "../view-controller.js"
+import{signUpSubmit} from "../controller/view-controller.js"
 
 export default ()=>{
     const form = document.createElement('div');
