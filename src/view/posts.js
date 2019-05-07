b2485286fd9fe4b63796f5b4423d2337a5971c22
LeslieZarate <fiorelly30zarate@gmail.com>
2019-05-07 12:37:24 -0500

    error

--- src/view/posts.js
+++ src/view/posts.js
@@ -2,50 +2,6 @@ import {addNotes} from "../view-controller.js"
 import {readNotes} from '../controller/controller-firebase.js'
 
 export default () => {
-<<<<<<< HEAD
-  const post = document.createElement('section');  
-  post.classList.add('posts-content','m1', 'p2');
-  const postContent = `
-   	<!-- FORMULARIO POST -->
-    <div class="form-post  p2 m1">
-      <form>
-        <textarea id="text-post"  placeholder="¿Qué estas pensando?"></textarea> 
-        <div class="btn-actions">
-          <select id="options-privacy">
-            <option value="public">Publico</option>
-            <option value="only-me">Privado</option>
-          </select>
-          <i id="btn-img" class="fas fa-image icons m1"></i>
-          <i id="btn-save" class="fas fa-paper-plane icons m1"></i>
-        </div>               
-      </form>					
-    </div>
-		<!-- POSTS -->
-		<h1>Publicaciones </h1>
-		<!-- TOTAL POST -->
-		<div class="public-posts">					
-			<div class="form-post m1 ">
-				<div class="user-post">
-					<p>Nombre user</p>
-					<i id= "btn-delete" class="fas fa-window-close icons"></i>
-		    </div>
-				<form class="p2">							
-					<textarea id="text-post"  placeholder="¿Qué estas pensando?" value="https"></textarea> 
-					<div class="btn-actions m1">	
-						<i id="btn-like" class="fas fa-heart icons m1">2</i>
-						<i id="btn-edit" class="fas fa-edit icons m1"></i>	
-						<i id="btn-save" class="fas fa-save icons m1"></i>							
-						</div>               
-				</form>					
-			</div>				
-		</div>
-    `;
-            
-            
-		post.innerHTML = postContent;		
-		const btnSave = post.querySelector('#btn-save');
-		btnSave.addEventListener('click',addNoteSubmit)
-=======
     const post = document.createElement('section');  
     post.classList.add('posts-content','m1', 'p2');
     const postContent = `
@@ -74,7 +30,6 @@ export default () => {
 		const btnSave = post.querySelector('#savePost');
 		btnSave.addEventListener('click',addNotes);
 		readNotes(templatePost);
->>>>>>> c8df0a323037819b32cf0a5ed606180522fcecdc
 
 		return post;
   }
